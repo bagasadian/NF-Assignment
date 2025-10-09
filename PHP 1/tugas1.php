b67a86f6a42a0a40da10a85f3dc74bfd2f1d6c89
Bagas Adi Anugerah <143944705+bagasadian@users.noreply.github.com>
2025-10-09 20:11:18 +0700

    TUGAS PHP 1

--- PHP 1/tugas1.php
+++ PHP 1/tugas1.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="id">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Form Nilai Ujian</title>
-</head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1 PHP</title>
 <body>
-    <h2>Form Penilaian Ujian</h2>
+    <h1>Form Penilaian Ujian</h1>
 
-    <!-- Form Input -->
-    <form method="POST" action="">
+    <!-- Form -->
+    <form method="post">
         <label>Nama:</label><br>
         <input type="text" name="nama" required><br><br>
 
@@ -16,7 +16,7 @@
         <input type="email" name="email" required><br><br>
 
         <label>Nilai Ujian:</label><br>
-        <input type="nilai" name="nilai" required><br><br>
+        <input type="number" name="nilai" required><br><br>
 
         <input type="submit" name="submit" value="Kirim">
     </form>
@@ -24,24 +24,22 @@
     <hr>
 
     <?php
-
-
-    // Form processing
+    // Proses form
     if (isset($_POST['submit'])) {
         $nama  = $_POST['nama'];
         $email = $_POST['email'];
         $nilai = $_POST['nilai'];
-        
-        echo "<h3>Hasil Penilaian</h3>";
+
+        echo "<h2>Hasil Penilaian</h2>";
         echo "Nama: $nama <br>";
         echo "Email: $email <br>";
         echo "Nilai Ujian: $nilai <br>";
 
         // Struktur kendali if-else
         if ($nilai > 70) {
-            echo "<strong>Status: Lulus </strong>";
+            echo "Status: Lulus";
         } else {
-            echo "<strong>Status: Remidi</strong>";
+            echo "Status: Remedial";
         }
     }
     ?>
